a0811abb3b6ba95efeae10ff62f93ac73d89e634
shinima <842351815@qq.com>
2017-06-27 15:33:51 +0800

    初步实现StaticmacMapping组件; 在storybook中添加StaticMacMapping;

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@storybook/react'
 
 function loadStories() {
   require('../app/stories/index.js');
+  require('../app/stories/StaticMacMapping')
   // You can require as many stories as you need.
 }
 
