86b845a9817296e4402637d512268c2345ab140a
Kong Lingling <wemmaling@163.com>
2017-06-27 20:33:47 +0800

    完善一些todo,maclist的数据结构重构tmp

--- app/components/MacList.jsx
+++ app/components/MacList.jsx
@@ -14,29 +14,27 @@ export default class MacList extends Component {
   handleAdd = () => {
     const { macName } = this.state
     const { maclist, validNameSet, existedMacSet } = this.props
+    const object = {
+      name: macName,
+      active: false,
+    }
     if (maclist.indexOf(macName) !== -1) {
       alert('该用户已存在列表中')
     } else if (validNameSet.has(macName)) {
-      this.props.addItem(macName)
+      this.props.addItem(object)
       this.setState({ macName: '' })
     } else if (!isValidMac(macName)) {
       alert('请输入正确的mac地址')
-      this.setState({ macName: '' })
     } else if (existedMacSet.has(macName)) {
       alert('mac地址重复')
-      this.setState({ macName: '' })
     } else {
-      this.props.addItem(macName)
+      this.props.addItem(object)
       this.setState({ macName: '' })
     }
   }
 
-  handleInputName = (e) => {
-    this.setState({ macName: e.target.value })
-  }
-
   render() {
-    const { maclist, checkedMacList } = this.props
+    const { maclist } = this.props
     const { macName } = this.state
     return (
       <div className="widgets">
@@ -46,22 +44,25 @@ export default class MacList extends Component {
             {maclist.map((mac, index) =>
               <div className="mac-item" key={index}>
                 <div className="mac-color">
-                  <div className="color" style={{ background: `${getColor(mac)}` }}>
+                  <div className="color" style={{ background: `${getColor(mac.name)}` }}>
                   </div>
                 </div>
-                <div className="mac-text">{mac}</div>
-                <button className="mac-delete" onClick={() => this.handleDelete(mac)}>Del
+                <div className="mac-text">{mac.name}</div>
+                <button className="mac-delete" onClick={() => this.handleDelete(mac.name)}>Del
                 </button>
                 <input type="checkbox"
-                       checked={checkedMacList.indexOf(mac) !== -1}
+                       checked={mac.active}
                        onChange={() => this.props.onToggleItem(mac)}
                 />
               </div>
             )}
           </div>
           <div className="new-mac-item">
-            <input type="text" placeholder="macName"
-                   onChange={this.handleInputName} value={macName} />
+            <input
+              type="text"
+              value={macName}
+              onChange={(e) => this.setState({ macName: e.target.value })}
+            />
             <button className="button" onClick={this.handleAdd}>Add</button>
           </div>
         </div>
--- app/components/MacListManager.jsx
+++ app/components/MacListManager.jsx
@@ -2,46 +2,70 @@ import React, { Component } from 'react'
 import MacList from './MacList'
 
 const validNameSet = new Set(['sfc-samsung', 'cx-meizu', 'lxy-meizu', 'a', 'b', 'c', 'bob'])
-const existedMacSet = new Set(['a0:b1:c2:d3:e4:f5','00-11-22-33-44-55','88:88:88:88:88:88'])
+const existedMacSet = new Set(['a0:b1:c2:d3:e4:f5', '00-11-22-33-44-55', '88:88:88:88:88:88'])
 
 export default class MacListManager extends Component {
   state = {
     validNameSet,
     existedMacSet,
-    maclist: ['bob', 'a', 'b', 'c'],
-    checkedMacList: ['a', 'b'],
+    // maclist: ['bob', 'a', 'b', 'c'],
+    // checkedMacList: ['a', 'b'],
+    // todo refactor data structure
+    maclist: [
+      {
+        name: 'bob',
+        active: false,
+      },
+      {
+        name: 'a',
+        active: true,
+      },
+      {
+        name: 'b',
+        active: true,
+      },
+      {
+        name: 'c',
+        active: false,
+      },
+    ],
   }
 
   deleteItem = (mac) => {
     const { maclist } = this.state
-    const index = maclist.indexOf(mac)
-    maclist.splice(index, 1)
-    this.setState({ maclist })
+    const macNameArray = maclist.map(item => item.name)
+    console.log(macNameArray)
+    const index = macNameArray.indexOf(mac)
+    const newMacList = maclist.slice(0, index).concat(maclist.slice(index + 1))
+    this.setState({ maclist: newMacList })
   }
 
   addItem = (object) => {
     const { maclist } = this.state
-    const newMacList = maclist.concat(object)
+    const newMacList = maclist.concat([object])
     this.setState({ maclist: newMacList })
   }
 
   toggleItem = (mac) => {
-    const { checkedMacList } = this.state
-    const index = checkedMacList.indexOf(mac)
-    if (index === -1) {
-      this.setState({ checkedMacList: checkedMacList.concat([mac]) })
-    } else {
-      checkedMacList.splice(index, 1)
-      this.setState({ checkedMacList })
-    }
+    const { maclist } = this.state
+    // todo 更改maclist中mac的active属性 如何操作object？？？
+    // const checkedMacList = maclist.filter(item => item.active).map(item => item.name)
+    // console.log(checkedMacList)
+    // const index = checkedMacList.indexOf(macName)
+    //
+    // if (index === -1) {
+    //   this.setState({ checkedMacList: checkedMacList.concat([macName]) })
+    // } else {
+    //   const newCheckedMacList = checkedMacList.slice(0, index).concat(checkedMacList.slice(index + 1))
+    //   this.setState({ checkedMacList: newCheckedMacList })
+    // }
   }
 
   render() {
-    const { maclist, checkedMacList, validNameSet, existedMacSet } = this.state
+    const { maclist, validNameSet, existedMacSet } = this.state
     return (
       <MacList
         maclist={maclist}
-        checkedMacList={checkedMacList}
         deleteItem={this.deleteItem}
         addItem={this.addItem}
         onToggleItem={this.toggleItem}
