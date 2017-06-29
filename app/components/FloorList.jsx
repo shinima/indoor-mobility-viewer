06e32cead88de4854adf3b44c5aadab383153061
Kong Lingling <wemmaling@163.com>
2017-06-29 21:11:29 +0800

    FloorListManager交互完成

--- app/components/FloorList.jsx
+++ app/components/FloorList.jsx
@@ -3,23 +3,32 @@ import '../styles/FloorList.styl'
 
 export default class FloorList extends Component {
   render() {
-    const { floorDataArray } = this.props
+    const { floorDataArray, selectedFloorId, changeSelectedFloorId } = this.props
+
     return (
       <div className="widgets">
         <div className="floor-list-widget">
           <div className="title">楼层地图切换</div>
           <div className="floor-list">
             {floorDataArray.map((item, index) => (
-              <div className="floor-item" key={index}>
+              <div
+                key={index}
+                className="floor-item"
+                onClick={() => changeSelectedFloorId(item.floorId)}
+              >
                 <div className="bar" style={{ background: 'rgba(0, 255, 0, 25)' }} />
                 <div className="floor-text">
                   <span>{item.buildingFloor}</span>
                   <span className="count">0</span>
                 </div>
-                <input className="floor-radio" type="radio" />
+                <input
+                  className="floor-radio"
+                  type="radio"
+                  checked={selectedFloorId === item.floorId}
+                  readOnly
+                />
               </div>
-            ))
-            }
+            ))}
           </div>
         </div>
       </div>
