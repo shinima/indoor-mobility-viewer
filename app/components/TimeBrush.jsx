64543dd7306380d811442abbac2e1f8c9b17f182
Kong Lingling <wemmaling@163.com>
2017-07-11 16:47:20 +0800

    时间控件开发(tmp)

--- app/components/TimeBrush.jsx
+++ app/components/TimeBrush.jsx
@@ -1,35 +1,39 @@
 import React, { Component } from 'react'
-import * as d3 from 'd3'
 import '../styles/BrushManager.styl'
 
 export default class TimeBrush extends Component {
+  state = {
+    cx: 11,
+    cursor: 'default',
+  }
+
+  getStartClient = (event) => {
+    const x = event.clientX
+    console.log('start', x)
+  }
+
   render() {
+    const { cx, cursor } = this.state
     return (
       <div className="duration-widget">
         <div className="title">时间控制</div>
-        <p className="duration">
-          <span>总时间区间</span>
-          <select>
-            <option value="1">1分钟</option>
-            <option value="5">5分钟</option>
-            <option value="60">1小时</option>
-            <option value="180">3小时</option>
-          </select>
-        </p>
-        <div className="span-info">
-          <p className="span-start">
-            <span className="text">起始时间</span>
-            <span className="value">startTime</span>
-          </p>
-          <p className="span-end">
-            <span className="text">终止时间</span>
-            <span className="value">endTime</span>
-          </p>
-          <p className="span-length">
-            <span className="text">时间区间</span>
-            <span className="value">timeLength</span>
-          </p>
-          <svg className="brush-container">???</svg>
+        <div className="time-selector">
+          <svg className="time-box" viewBox="0 0 120 4 ">
+            <rect x="10" y="1" width="100" height="1" fill="white" stroke="steelblue"
+                  strokeWidth="0.1" cursor={cursor}
+                  onMouseDown={(event) => this.setState({ cx: event.clientX / 10 })}
+                  onMouseOver={() => this.setState({ cursor: 'pointer' })}
+            />
+            <circle
+              id="circle"
+              cx={cx} cy="1.5" r="1" fill="steelblue" stroke="steelblue" strokeWidth="0.1"
+              cursor={cursor}
+              draggable={true}
+              onMouseDown={this.getStartClient}
+              // onMouseUp={this.handleMouseUp}
+              onMouseOver={() => this.setState({ cursor: 'move' })}
+            />
+          </svg>
         </div>
       </div>
     )
